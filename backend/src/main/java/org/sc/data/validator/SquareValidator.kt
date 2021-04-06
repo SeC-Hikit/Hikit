c19b1d25d158314fada44a89acd990e6d329e3cb
Sara <milner.saragiulia@gmail.com>
2021-04-06 18:19:33 +0200

    #67 Added Endpoint to geolocate trails within a square - Validator (we are simplifying by considering a square instead of a rectangle)

--- backend/src/main/java/org/sc/data/validator/SquareValidator.kt
+++ backend/src/main/java/org/sc/data/validator/SquareValidator.kt
@@ -12,19 +12,23 @@ import java.util.*
 class SquareValidator : Validator<SquareDto> {
 
     companion object {
-        const val topBottomLeftError = "Distance bottom-top left vertex is greater than 60 km!"
-        const val topBottomRightError = "Distance bottom-top right vertex is greater than 60 km!"
+        const val vertexDistanceError = "Distance between selected vertexes is greater than 100 km!"
     }
 
     override fun validate(request: SquareDto): Set<String> {
         val errors = mutableSetOf<String>()
         if (DistanceProcessor.getRadialDistance(request.bottomLeft.latitude,request.bottomLeft.longitude,
-                        request.topLeft.latitude,request.topLeft.longitude) > 60000)
-                        {errors.add(topBottomLeftError)}
+                        request.topLeft.latitude,request.topLeft.longitude) > 100000)
+                        {errors.add(vertexDistanceError)}
         if (DistanceProcessor.getRadialDistance(request.bottomRight.latitude,request.bottomRight.longitude,
-                        request.topRight.latitude,request.topRight.longitude) > 60000)
-                        {errors.add(topBottomRightError)}
-
+                        request.topRight.latitude,request.topRight.longitude) > 100000)
+                        {errors.add(vertexDistanceError)}
+        if (DistanceProcessor.getRadialDistance(request.bottomRight.latitude,request.bottomRight.longitude,
+                        request.bottomLeft.latitude,request.bottomLeft.longitude) > 100000)
+                         {errors.add(vertexDistanceError)}
+        if (DistanceProcessor.getRadialDistance(request.topLeft.latitude,request.topLeft.longitude,
+                        request.topRight.latitude,request.topRight.longitude) > 100000)
+                        {errors.add(vertexDistanceError)}
         return errors
     }
 }
