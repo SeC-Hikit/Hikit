5d33479eb3f49b54ed848898e29cb88dfca517a2
loreV <lore.verri@gmail.com>
2021-04-07 11:59:03 +0200

    #67 Added coordinates validation

--- backend/src/main/java/org/sc/data/validator/SquareValidator.kt
+++ backend/src/main/java/org/sc/data/validator/SquareValidator.kt
@@ -1,34 +1,45 @@
 package org.sc.data.validator
 
-import org.apache.commons.lang3.StringUtils.isEmpty
-import org.sc.common.rest.AccessibilityNotificationCreationDto
+import org.sc.common.rest.CoordinatesDto
 import org.sc.common.rest.geo.SquareDto
-import org.sc.data.model.Coordinates2D
 import org.sc.processor.DistanceProcessor
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
-class SquareValidator : Validator<SquareDto> {
+class SquareValidator constructor(private val coordinatesValidator: CoordinatesValidator) : Validator<SquareDto> {
 
     companion object {
+        const val UPPER_BOUND_ONE_HUNDRED_KM = 100000
         const val vertexDistanceError = "Distance between selected vertexes is greater than 100 km!"
     }
 
     override fun validate(request: SquareDto): Set<String> {
         val errors = mutableSetOf<String>()
-        if (DistanceProcessor.getRadialDistance(request.bottomLeft.latitude,request.bottomLeft.longitude,
-                        request.topLeft.latitude,request.topLeft.longitude) > 100000)
-                        {errors.add(vertexDistanceError)}
-        if (DistanceProcessor.getRadialDistance(request.bottomRight.latitude,request.bottomRight.longitude,
-                        request.topRight.latitude,request.topRight.longitude) > 100000)
-                        {errors.add(vertexDistanceError)}
-        if (DistanceProcessor.getRadialDistance(request.bottomRight.latitude,request.bottomRight.longitude,
-                        request.bottomLeft.latitude,request.bottomLeft.longitude) > 100000)
-                         {errors.add(vertexDistanceError)}
-        if (DistanceProcessor.getRadialDistance(request.topLeft.latitude,request.topLeft.longitude,
-                        request.topRight.latitude,request.topRight.longitude) > 100000)
-                        {errors.add(vertexDistanceError)}
+        if (DistanceProcessor.getRadialDistance(request.bottomLeft.latitude, request.bottomLeft.longitude,
+                        request.topLeft.latitude, request.topLeft.longitude) > UPPER_BOUND_ONE_HUNDRED_KM) {
+            errors.add(vertexDistanceError)
+        }
+        if (DistanceProcessor.getRadialDistance(request.bottomRight.latitude, request.bottomRight.longitude,
+                        request.topRight.latitude, request.topRight.longitude) > UPPER_BOUND_ONE_HUNDRED_KM) {
+            errors.add(vertexDistanceError)
+        }
+        if (DistanceProcessor.getRadialDistance(request.bottomRight.latitude, request.bottomRight.longitude,
+                        request.bottomLeft.latitude, request.bottomLeft.longitude) > UPPER_BOUND_ONE_HUNDRED_KM) {
+            errors.add(vertexDistanceError)
+        }
+        if (DistanceProcessor.getRadialDistance(request.topLeft.latitude, request.topLeft.longitude,
+                        request.topRight.latitude, request.topRight.longitude) > UPPER_BOUND_ONE_HUNDRED_KM) {
+            errors.add(vertexDistanceError)
+        }
+
+        val errorsOnCoordinates = listOf(request.bottomLeft, request.topLeft,
+                request.topRight, request.bottomRight).flatMap {
+            coordinatesValidator
+                    .validate(CoordinatesDto(it.latitude, it.longitude))
+        }
+
+        errors.addAll(errorsOnCoordinates)
+
         return errors
     }
 }
