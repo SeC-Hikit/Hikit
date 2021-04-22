69409c447aafa077e514eaa2036dc633acc97e79
loreV <lore.verri@gmail.com>
2021-04-23 01:14:28 +0200

    #52 Added check on authority

---
+++ backend/src/main/java/org/sc/configuration/SecurityConfig.java
@@ -0,0 +1,108 @@
+package org.sc.configuration;
+
+import org.apache.logging.log4j.Logger;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.io.InputStream;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    private static final Logger LOGGER = getLogger(SecurityConfig.class);
+
+    public static final String ALL_ALLOWED_WILDCARD = "/*";
+    public static final String AUTHENTICATION_IS_DISABLED_MESSAGE = "Authentication is disabled";
+    @Autowired
+    AppProperties appProperties;
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
+        grantedAuthorityMapper.setPrefix("ROLE_");
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    @Override
+    @ConditionalOnMissingBean(HttpSessionManager.class)
+    protected HttpSessionManager httpSessionManager() {
+        return new HttpSessionManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        if (!appProperties.getEnabledSecurity()) {
+            LOGGER.warn(AUTHENTICATION_IS_DISABLED_MESSAGE);
+            http.authorizeRequests().antMatchers(ALL_ALLOWED_WILDCARD).permitAll();
+        } else {
+            http.csrf().disable().cors().disable().authorizeRequests()
+                    .antMatchers("/trail*").hasAuthority("test-role")
+                    .anyRequest().authenticated();
+        }
+    }
+
+    /**
+     * Overrides default keycloak config resolver behaviour (/WEB-INF/keycloak.json) by a simple mechanism.
+     * <p>
+     * This example loads other-keycloak.json when the parameter use.other is set to true, e.g.:
+     * {@code ./gradlew bootRun -Duse.other=true}
+     *
+     * @return keycloak config resolver
+     */
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakConfigResolver() {
+
+            private KeycloakDeployment keycloakDeployment;
+
+            @Override
+            public KeycloakDeployment resolve(HttpFacade.Request facade) {
+                if (keycloakDeployment != null) {
+                    return keycloakDeployment;
+                }
+
+                String path = "/keycloak.json";
+                InputStream configInputStream = getClass().getResourceAsStream(path);
+
+                if (configInputStream == null) {
+                    throw new RuntimeException("Could not load Keycloak deployment info: " + path);
+                } else {
+                    keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+                }
+
+                return keycloakDeployment;
+            }
+        };
+    }
+}
