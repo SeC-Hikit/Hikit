25251edd80fb76b4dfadbfcb0ccd7083272a02ac
loreV <lore.verri@gmail.com>
2021-06-29 22:29:36 +0200

    #85 Simple refactoring

--- backend/src/main/java/org/sc/processor/TrailSimplifier.java
+++ backend/src/main/java/org/sc/processor/TrailSimplifier.java
@@ -16,18 +16,19 @@ public class TrailSimplifier {
     public static final int POSITIVE_SCALAR = 1000000;
     public static final float SUPER_LOW_Q_TOLERANCE = 50f;
     public static final float LOW_Q_TOLERANCE = 20f;
-    public static final float MEDIUM_Q_TOLERANCE = 5f;
+    public static final float MEDIUM_Q_TOLERANCE = 5f;// Douglas-Peucker, false for Radial-Distance
+    public static final boolean IS_DOUGLAS_PECKER_ALGORITHM = true;
 
-    public List<TrailCoordinates> simplify(List<TrailCoordinates> allcoordinates,
+    public List<TrailCoordinates> simplify(List<TrailCoordinates> allCoordinates,
                                            TrailSimplifierLevel compressionLevel){
 
         // create an instance of the simplifier (empty array needed by List.toArray)
-        TrailCoordinates[] trailCoordinates = new TrailCoordinates[allcoordinates.size()];
-        Simplify<TrailCoordinates> simplify = new Simplify<>(new TrailCoordinates[0], latLngPointExtractor);
+        final TrailCoordinates[] trailCoordinates = new TrailCoordinates[allCoordinates.size()];
+        final Simplify<TrailCoordinates> simplify = new Simplify<>(new TrailCoordinates[0], latLngPointExtractor);
 
         // here we have an array with hundreds of points
         //need to convert list to array, since is needed by the pkg
-        allcoordinates.toArray(trailCoordinates);
+        allCoordinates.toArray(trailCoordinates);
 
         //float tolerance = 2f;
         float tolerance = 0f;
@@ -40,10 +41,8 @@ public class TrailSimplifier {
            tolerance = MEDIUM_Q_TOLERANCE;
         }
 
-        boolean highQuality = true; // Douglas-Peucker, false for Radial-Distance
-
         // run simplification process
-        TrailCoordinates[] lessPoints = simplify.simplify(trailCoordinates, tolerance, highQuality);
+        final TrailCoordinates[] lessPoints = simplify.simplify(trailCoordinates, tolerance, IS_DOUGLAS_PECKER_ALGORITHM);
         return Arrays.stream(lessPoints).filter(Objects::nonNull).collect(Collectors.toList());
     }
     private static final PointExtractor<TrailCoordinates> latLngPointExtractor = new PointExtractor<TrailCoordinates>() {
