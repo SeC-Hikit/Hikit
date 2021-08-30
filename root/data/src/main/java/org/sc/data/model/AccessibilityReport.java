f3ab8da9f2dcaec9b7f73d20c7b478cfa741533b
Lorenzo Verri <lore.verri@gmail.com>
2021-08-30 18:28:52 +0200

    #92 Added model entries

---
+++ root/data/src/main/java/org/sc/data/model/AccessibilityReport.java
@@ -0,0 +1,35 @@
+package org.sc.data.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessibilityReport {
+
+    public final static String COLLECTION_NAME = "core.AccessibilityReports";
+
+    public final static String ID = "_id";
+    public final static String TRAIL_ID = "trailId";
+    public final static String EMAIL = "email";
+    public final static String TELEPHONE = "telephone";
+    public final static String DESCRIPTION = "description";
+    public final static String REPORT_DATE = "reportDate";
+    public final static String ISSUE_ID = "issuedId";
+    public final static String COORDINATES = "coordinates";
+    public final static String RECORD_DETAILS = "recordDetails";
+
+    private String id;
+    private String description;
+    private String trailId;
+    private String email;
+    private String telephone;
+    private Date reportDate;
+    private String issueId;
+    private TrailCoordinates coordinates;
+    private RecordDetails recordDetails;
+}
---
+++ root/rest/src/main/java/org/sc/common/rest/AccessibilityReportDto.java
@@ -0,0 +1,20 @@
+package org.sc.common.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class AccessibilityReportDto {
+    private String id;
+    private String description;
+    private String trailId;
+    private String email;
+    private String telephone;
+    private String issueId;
+    private Date reportDate;
+    private CoordinatesDto coordinates;
+    private RecordDetailsDto recordDetails;
+}
