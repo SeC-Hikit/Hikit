b07dc7f1943e378b812f24cabe5df5b54270f6ff
Lorenzo Verri <lore.verri@gmail.com>
2021-09-14 00:03:58 +0200

    #92 Changed parameter

--- backend/src/main/java/org/sc/adapter/mail/impl/AccessibilityReportMailAdapter.kt
+++ backend/src/main/java/org/sc/adapter/mail/impl/AccessibilityReportMailAdapter.kt
@@ -12,16 +12,19 @@ class AccessibilityReportMailAdapter @Autowired constructor(private val mailAdap
     companion object {
         const val subject = "[S&C] Attivazione segnalazione"
         val template = String.format(MailAdapter.HTML_TEMPLATE, "Il %s, hai inviato una segnalazione " +
-                "tramite S&C al '%s' per il sentiero '%s'. </br> Descrizione: '%s'.<br/>Per attivare la segnalazione, clicca <a href='%s'>qui</a>, " +
+                "tramite S&C al '%s' per il sentiero '%s'. </br> Descrizione: '%s'.<br/>Per attivare la segnalazione, clicca <a href='%s' target='_blank'>qui</a>, " +
                 "oppure copia incolla questo indirizzo nella barra indirizzi del browser: %s")
     }
 
     fun sendValidation(reportDate: Date, trailId: String,
                        realm: String, description: String,
-                       activationId: List<String>, targetEmailAddress: String) {
-        val body = String.format(template, mailAdapter.dateFormatter.format(reportDate),
+                       activationId: String, targetEmailAddress: String) {
+        val activationLink = mailAdapter.activationAddress + activationId
+        val body = String.format(
+                template, mailAdapter.dateFormatter.format(reportDate),
                 realm, trailId, description,
-                mailAdapter.activationAddress + activationId)
+                activationLink,
+        )
         mailAdapter.send(subject, body, targetEmailAddress)
     }
 }
