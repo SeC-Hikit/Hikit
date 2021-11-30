cc948c394273b55a5b89a51b3a82dc1437481a29
Lorenzo Verri <lore.verri@gmail.com>
2021-11-30 22:59:52 +0100

    #120 Added further table to EVO that used to make use of the wrong naming

--- evo/01_#120.js
+++ evo/01_#120.js
@@ -1,4 +1,4 @@
-let collections = ["core.Media", "core.Trail"];
+let collections = ["core.Media", "core.Trail", "core.Raw"];
 
 collections.forEach((collection)=> {
     db.getCollection(collection).updateMany({}, {$rename:{"fileDetails": "recordDetails"}})
