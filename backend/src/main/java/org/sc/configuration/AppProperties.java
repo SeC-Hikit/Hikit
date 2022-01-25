5909bfe4c57c39a21ef43be0fa1f96258f9d03fc
Lorenzo Verri <lore.verri@gmail.com>
2022-01-25 15:48:15 +0100

    #149 Release 1.2

--- backend/src/main/java/org/sc/configuration/AppProperties.java
+++ backend/src/main/java/org/sc/configuration/AppProperties.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AppProperties {
 
     public static final String APP_NAME = "Sentieri&Cartografia";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     public static final String LOCAL_IP_ADDRESS = "127.0.0.1";
 
     private final String port;
