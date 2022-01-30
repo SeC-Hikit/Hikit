354fadd4118fcf7858a9096e6b54ea65fe0e7bd1
Lorenzo Verri <lore.verri@gmail.com>
2022-01-30 16:33:44 +0100

    #148 Added distance between coordinates endpoint

--- backend/src/main/java/org/sc/controller/GeoToolsController.java
+++ backend/src/main/java/org/sc/controller/GeoToolsController.java
@@ -4,10 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.sc.adapter.AltitudeServiceAdapter;
 import org.sc.common.rest.CoordinatesDto;
 import org.sc.data.validator.GeneralValidator;
+import org.sc.manager.GeoToolManager;
+import org.sc.processor.DistanceProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(GeoToolsController.PREFIX)
@@ -16,14 +21,14 @@ public class GeoToolsController {
     public final static String PREFIX = "/geo-tool";
 
     private final GeneralValidator generalValidator;
-    private final AltitudeServiceAdapter altitudeServiceAdapter;
+    private final GeoToolManager geoToolManager;
 
 
     @Autowired
     public GeoToolsController(final GeneralValidator generalValidator,
-                              final AltitudeServiceAdapter altitudeServiceAdapter) {
+                              final GeoToolManager geoToolManager) {
         this.generalValidator = generalValidator;
-        this.altitudeServiceAdapter = altitudeServiceAdapter;
+        this.geoToolManager = geoToolManager;
     }
 
     @Operation(summary = "Find a point elevation by lat-long")
@@ -34,7 +39,18 @@ public class GeoToolsController {
         if(!errors.isEmpty()) {
             return new CoordinatesDto();
         }
-        final double altitudeByLongLat = altitudeServiceAdapter.getAltitudeByLongLat(latitude, longitude);
+        final double altitudeByLongLat = geoToolManager.getAltitudeByLongLat(latitude, longitude);
         return new CoordinatesDto(latitude, longitude, altitudeByLongLat);
+    }
+
+    @Operation(summary = "Find coordinates distance")
+    @PostMapping("/distance")
+    public Double radialDistance(@RequestBody List<CoordinatesDto> coordinatesDtoList) {
+        final Set<String> errors = coordinatesDtoList.stream().map(generalValidator::validate)
+                .flatMap(Collection::stream).collect(Collectors.toSet());
+        if(!errors.isEmpty()) {
+            return (double) 0;
+        }
+        return geoToolManager.getDistanceBetweenCoordinates(coordinatesDtoList);
     }
 }
---
+++ backend/src/main/java/org/sc/manager/GeoToolManager.kt
@@ -0,0 +1,18 @@
+package org.sc.manager
+
+import org.sc.adapter.AltitudeServiceAdapter
+import org.sc.common.rest.CoordinatesDto
+import org.sc.processor.TrailsStatsCalculator
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class GeoToolManager @Autowired constructor(private val altitudeService : AltitudeServiceAdapter,
+                                            private val trailsStatsCalculator: TrailsStatsCalculator) {
+
+    fun getAltitudeByLongLat(latitude: Double, longitude: Double) =
+            altitudeService.getAltitudeByLongLat(latitude, longitude)
+
+    fun getDistanceBetweenCoordinates(coords: List<CoordinatesDto>) : Double =
+            trailsStatsCalculator.calculateTrailLength(coords)
+}
