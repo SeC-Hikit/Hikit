17154c07036241f15aab5d9ab82a2fc11b73d35b
Lorenzo Verri <lore.verri@gmail.com>
2022-01-31 00:52:27 +0100

    #156 Fixed and clean imports

--- backend/src/main/java/org/sc/data/validator/PlaceRefValidator.kt
+++ backend/src/main/java/org/sc/data/validator/PlaceRefValidator.kt
@@ -1,15 +1,12 @@
 package org.sc.data.validator
 
-import org.apache.commons.lang3.StringUtils
 import org.sc.common.rest.PlaceRefDto
-import org.sc.manager.PlaceManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class PlaceRefValidator @Autowired constructor(
-    private val trailCoordinatesCreationValidator: CoordinatesValidator,
-    private val placeManager: PlaceManager
+        private val trailCoordinatesCreationValidator: CoordinatesValidator
 ) : Validator<PlaceRefDto> {
 
     override fun validate(request: PlaceRefDto): Set<String> {
