3b4f2ae2c8462e215a7d271d247ed608fe0e99b9
Lorenzo Verri <lore.verri@gmail.com>
2023-01-08 21:29:10 +0100

    #235 Completed Dao

--- backend/src/main/java/org/sc/data/repository/AnnouncementsDao.kt
+++ backend/src/main/java/org/sc/data/repository/AnnouncementsDao.kt
@@ -3,10 +3,12 @@ package org.sc.data.repository
 import com.mongodb.client.MongoCollection
 import org.apache.logging.log4j.LogManager
 import org.bson.Document
+import org.bson.types.ObjectId
 import org.sc.configuration.DataSource
 import org.sc.data.entity.mapper.AnnouncementMapper
 import org.sc.data.model.Announcement
 import org.sc.data.model.RecordDetails
+import org.sc.data.model.Trail
 import org.sc.data.repository.MongoUtils.DESCENDING_ORDER
 import org.sc.data.repository.MongoUtils.DOT
 import org.springframework.stereotype.Repository
@@ -20,8 +22,10 @@ class AnnouncementsDao(dataSource: DataSource,
     val collection: MongoCollection<Document>
 
     init {
-        collection = dataSource.getDB().getCollection(Announcement.COLLECTION_NAME)
+        collection = dataSource.db.getCollection(Announcement.COLLECTION_NAME)
     }
+
+    fun get(id: String) = toAnnouncementList(collection.find(Document(Announcement.ID, id)))
 
     fun get(skip: Int,
             limit: Int,
@@ -32,6 +36,28 @@ class AnnouncementsDao(dataSource: DataSource,
                 .limit(limit)
                 .sort(Document(Announcement.RECORD_DETAILS + DOT + RecordDetails.UPLOADED_ON,
                         DESCENDING_ORDER)))
+    }
+
+    fun create(announcement: Announcement): List<Announcement> {
+        val objectId = ObjectId().toHexString()
+        collection.findOneAndReplace(
+                Document(Trail.ID, objectId),
+                announcementsMapper.mapToDocument(announcement.copy(id = objectId))
+        )
+        return get(objectId)
+    }
+
+    fun update(update: Announcement): List<Announcement> {
+        val updateOne = collection.updateOne(
+                Document(Announcement.ID, update.id),
+                announcementsMapper.mapToDocument(update)
+        )
+        if (!updateOne.wasAcknowledged()) logger.error("Could not delete announcement with id: ${update.id}")
+        return get(update.id)
+    }
+
+    fun delete(id: String) {
+        collection.deleteOne(Document(Announcement.ID, id))
     }
 
     private fun toAnnouncementList(documents: Iterable<Document>): List<Announcement> {
