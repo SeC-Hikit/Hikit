0a0d8d8c6383fdde6aab92981e0bedd4a174e30a
Lorenzo Verri <lore.verri@gmail.com>
2025-08-07 15:35:53 +0200

    feat(277): add impl

--- root/rest/src/main/java/org/sc/common/rest/response/MunicipalityIntersectionResponse.kt
+++ root/rest/src/main/java/org/sc/common/rest/response/MunicipalityIntersectionResponse.kt
@@ -1,7 +1,5 @@
 package org.sc.common.rest.response
 
 import org.sc.common.rest.MunicipalityToTrailDto
-import org.sc.common.rest.Status
-import org.sc.common.rest.TrailIntersectionDto
 
 data class MunicipalityIntersectionResponse (val content: List<MunicipalityToTrailDto>)
