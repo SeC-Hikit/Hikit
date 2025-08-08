2749d2bf099523d10b9003b67d237d05eca5b098
Lorenzo Verri <lore.verri@gmail.com>
2025-08-08 18:14:28 +0200

    feat(278): add impl

--- backend/src/main/java/org/sc/service/TrailService.kt
+++ backend/src/main/java/org/sc/service/TrailService.kt
@@ -1,5 +1,6 @@
 package org.sc.service
 
+import org.openapitools.model.CityRefDto
 import org.openapitools.model.MunicipalityDto
 import org.sc.adapter.AltitudeServiceAdapter
 import org.sc.adapter.microservice.ErtMunicipalityMicroserviceAdapter
@@ -12,12 +13,13 @@ import org.sc.data.model.MunicipalityDetails
 import org.sc.data.model.TrailStatus
 import org.sc.job.import.MunicipalityForTrailsImporter
 import org.sc.manager.*
-import org.sc.processor.PlacesTrailSyncProcessor
-import org.sc.processor.TrailSimplifierLevel
-import org.sc.processor.TrailsStatsCalculator
+import org.sc.processor.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.logging.Logger
+
+private const val unknown = "unknown"
+private const val intersection = "intersection"
 
 @Service
 class TrailService @Autowired constructor(
@@ -129,21 +131,6 @@ class TrailService @Autowired constructor(
         val byId = trailManager.getById(trailId, TrailSimplifierLevel.HIGH)
         val trail = byId.first()
 
-
-        // In case only one municipality, return the whole distance
-        if (trail.municipalities.size == 1) {
-
-            val municipality = municipalityMicroserviceAdapter.getByName(trail.municipalities.first().city)
-            val municipalityFull = municipality!!.body!!.content.first()
-            return listOf(
-                MunicipalityToTrailDto(
-                    emptyList(),
-                    trail.statsTrailMetadata.length, trail.municipalities.first(),
-                    municipalityFull.geometry.map { Coordinates2D(it.longitude, it.latitude) }
-                )
-            )
-        }
-
         val municipalityToIntersectingPoints =
             trailIntersector.findIntersectionWithMunicipalities(trailMapper.map(trail))
 
@@ -152,54 +139,126 @@ class TrailService @Autowired constructor(
         return calculateDistances(municipalityToIntersectingPoints, trail)
     }
 
+    private fun mapToElevationCoord(it: Coordinates2D): CoordinatesDto {
+        return CoordinatesDto(
+            it.latitude,
+            it.longitude,
+            altitudeServiceAdapter.getElevationsByLongLat(it.longitude, it.latitude)[0]
+        )
+    }
+
     private fun calculateDistances(
         municipalityToIntersectingPoints: List<Pair<MunicipalityDto, List<Coordinates2D>>>,
         trail: TrailDto
-    ): List<MunicipalityToTrailDto> = municipalityToIntersectingPoints.map {
-        // Got indexes to place points
-        val calculatedIndexesDistances = it.second.map { targetCoord ->
+    ): List<MunicipalityToTrailDto> {
+        val municipalityToIntersectionEnriched = municipalityToIntersectingPoints
+            .plus(getMunicipality(unknown))
+            .plus(getMunicipality(intersection))
+        val municipalityToDistance = mutableMapOf<String, Double>()
+        val municipalities = municipalityToIntersectionEnriched.map {
             Pair(
-                targetCoord, trailsStatsCalculator.getLowestCumulativeDistanceAndIndexForCoordinate(
-                    trail.coordinates,
-                    CoordinatesDto(targetCoord.latitude, targetCoord.longitude)
-                )
-            )
-        }.requireNoNulls()
-
-
-        // Add points to construct a line including them
-        val coordinates = trail.coordinates
-        calculatedIndexesDistances.forEach { coordsToIndex ->
-            val index = coordsToIndex.second!!.second
-            val lat = coordsToIndex.first.latitude
-            val long = coordsToIndex.first.longitude
-            coordinates.add(
-                index, TrailCoordinatesDto(
-                    lat, long,
-                    altitudeServiceAdapter.getElevationsByLongLat(lat, long).first(), 0
-                )
-            )
-        }
-
-        val fromTo = calculatedIndexesDistances.map { coordsToDistanceAndIndex -> coordsToDistanceAndIndex.first }
-        val distance =
-            trailsStatsCalculator.calculateTrailLengthFromToPoint(coordinates, fromTo)
-        val intersectionPointsWithElevation = calculatedIndexesDistances.map { cx ->
-            coordinates[cx.second!!.second]
-        }
-
-        MunicipalityToTrailDto(
-            intersectionPointsWithElevation.map { c ->
-                CoordinatesDto(c.latitude, c.longitude, c.altitude)
-            },
-            distance,
-            MunicipalityDetailsDto(
-                it.first.id,
                 it.first.name,
-                it.first.relatingCity.province,
-                it.first.relatingCity.provinceShort
-            ),
-            it.first.geometry.map { c -> Coordinates2D(c.longitude, c.latitude) }
-        )
+                it.first.geometry.map { coord -> Coordinates2D(coord.longitude, coord.latitude) })
+        }
+        municipalities.forEach { municipalityToDistance[it.first] = 0.0 }
+
+        trail.coordinates.forEachIndexed { index, coord ->
+
+            if (index < trail.coordinates.size - 1) {
+                val pointWithinPolygon = GeoCalculator.isPointWithinPolygon(
+                    Coordinates2D(coord.longitude, coord.latitude),
+                    municipalities
+                )
+                val nextCoord = trail.coordinates[index + 1]
+                val nextPointWithinPolygon = GeoCalculator.isPointWithinPolygon(
+                    Coordinates2D(nextCoord.longitude, nextCoord.latitude),
+                    municipalities
+                )
+
+                // If both in same municipality
+                if (pointWithinPolygon.first == nextPointWithinPolygon.first) {
+                    val d: Double = municipalityToDistance[pointWithinPolygon.first]!!
+                    municipalityToDistance[pointWithinPolygon.first] =
+                        d + DistanceProcessor.distanceBetweenPoints(coord, nextCoord)
+                // Intersection of known areas
+                } else if (pointWithinPolygon.first != unknown && nextPointWithinPolygon.first != unknown) {
+                    municipalityToDistance[intersection] =
+                        municipalityToDistance[intersection]!! + DistanceProcessor.distanceBetweenPoints(coord, nextCoord)
+                // Intersection with unknown areas
+                } else {
+                    municipalityToDistance[unknown] =
+                        municipalityToDistance[unknown]!! + DistanceProcessor.distanceBetweenPoints(coord, nextCoord)
+                }
+            }
+
+
+        }
+
+        return municipalities.map { m ->
+            val municipalityToTrailDto = municipalityToIntersectionEnriched.filter { it.first.name.equals(m.first) }[0]
+
+            MunicipalityToTrailDto(
+                municipalityToTrailDto.second.map { mapToElevationCoord(it) },
+                municipalityToDistance[m.first],
+                MunicipalityDetailsDto(
+                    municipalityToTrailDto.first.id,
+                    municipalityToTrailDto.first.name,
+                    municipalityToTrailDto.first.relatingCity.province,
+                    municipalityToTrailDto.first.relatingCity.provinceShort
+                ),
+                municipalityToTrailDto.first.geometry.map { c -> Coordinates2D(c.longitude, c.latitude) }
+            )
+        }
     }
+
+    private fun getMunicipality(name: String): Pair<MunicipalityDto, List<Coordinates2D>> {
+        val first = MunicipalityDto()
+        first.name = name
+        first.id = ""
+        val cityRefDto = CityRefDto()
+        cityRefDto.province = name
+        cityRefDto.provinceShort = name
+        first.relatingCity = cityRefDto
+        first.geometry = emptyList()
+        return Pair(first, emptyList())
+    }
+
+
+//        municipalityToIntersectingPoints.map {
+//
+//
+//        // Got indexes to place points
+//        val calculatedIndexesDistances = it.second.map { targetCoord ->
+//            Pair(
+//                targetCoord, trailsStatsCalculator.getLowestCumulativeDistanceAndIndexForCoordinate(
+//                    trail.coordinates,
+//                    CoordinatesDto(targetCoord.latitude, targetCoord.longitude)
+//                )
+//            )
+//        }.requireNoNulls()
+//
+//
+//        // Add points to construct a line including them
+//        val coordinates = trail.coordinates
+//        calculatedIndexesDistances.forEach { coordsToIndex ->
+//            val index = coordsToIndex.second!!.second
+//            val lat = coordsToIndex.first.latitude
+//            val long = coordsToIndex.first.longitude
+//            coordinates.add(
+//                index, TrailCoordinatesDto(
+//                    lat, long,
+//                    altitudeServiceAdapter.getElevationsByLongLat(lat, long).first(), 0
+//                )
+//            )
+//        }
+//
+//        val fromTo = calculatedIndexesDistances.map { coordsToDistanceAndIndex -> coordsToDistanceAndIndex.first }
+//        val distance =
+//            trailsStatsCalculator.calculateTrailLengthFromToPoint(coordinates, fromTo)
+//        val intersectionPointsWithElevation = calculatedIndexesDistances.map { cx ->
+//            coordinates[cx.second!!.second]
+//        }
+//
+//
+
 }
